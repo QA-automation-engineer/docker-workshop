0ce3a114185d1eb4c340552b2f7de9fb101c7688
Vladimir Trandafilov <trandafilov.vladimir@gmail.com>
2019-02-12 11:19:46 +0200

    initial commit

---
+++ src/main/java/dockerworkshop/pages/BasePage.java
@@ -0,0 +1,20 @@
+package dockerworkshop.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import dockerworkshop.utils.SimpleAPI;
+
+public abstract class BasePage extends SimpleAPI {
+
+    protected WebDriver wd;
+
+    protected BasePage(WebDriver wd){
+        this.wd = wd;
+        PageFactory.initElements(wd, this);
+    }
+
+    @Override
+    protected WebDriver getWebDriver() {
+        return wd;
+    }
+}
